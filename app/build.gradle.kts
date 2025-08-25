a5689edd7d5f6d01bbb6f7a003d51499fddb3259
Ayush Chaudhary <ayush0000ayush@gmail.com>
2025-08-26 04:12:13 +0530

    :bulb: Btn to ask for more task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.blurr.voice"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.9" +
+        versionCode = 10
+        versionName = "1.0.10" +
                 ""
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
